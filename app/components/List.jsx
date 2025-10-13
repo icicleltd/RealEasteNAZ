9dcb005075d2288f928b018dc57c3d763ae82138
meghla <meghla@iciclecorporation.com>
2025-10-13 12:36:39 +0600

    add add all changes

--- app/components/List.jsx
+++ app/components/List.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 
 const images = [
-    "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg",
-    "/6.jpg", "/7.jpg", "/8.jpg", "/9.jpg",
+    "/2.jpg", "/3.jpg", "/4.jpg", "/5.jpg", "/10.jpg", "/1.jpg",
+    "/6.jpg", "/7.jpg", "/8.jpg", "/9.jpg", "/11.jpg", "/12.jpg"
 ];
 
 const List = () => {
